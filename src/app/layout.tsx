81681df1251b39bf2d9f4f879f4455d53d6adb1b
Xander Vanparys <xander.vanparys@gmail.com>
2025-07-16 10:39:03 +0200

    added apple icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Raketcounter" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
