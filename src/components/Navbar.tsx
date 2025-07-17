75a34f62ad9f6fd1948643819b3014f8176eef27
Xander Vanparys <xander.vanparys@gmail.com>
2025-07-17 23:18:42 +0200

    mobile navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -169,7 +169,7 @@ export default function Navbar() {
                 Leaderboard
               </Link>
               <Link
-                href="/ndboard"
+                href="/ndbord"
                 className="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400 block px-3 py-2 rounded-md text-base font-medium transition-colors duration-200"
                 onClick={() => setMobileOpen(false)}
               >
